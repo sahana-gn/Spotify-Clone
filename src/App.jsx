6ae82c3ed9c6e496e8761b7d50367f31574b4605
sahana-gn <142797004+sahana-gn@users.noreply.github.com>
2024-06-21 17:15:25 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React, { useContext } from 'react';
+import SideBar from './components/SideBar';
+import Player from './components/Player';
+import Display from './components/Display';
+import { PlayerContext } from './context/PlayerContext';
+
+const App=()=>{
+
+  const {audioref,track}=useContext(PlayerContext)
+  return(
+    <div className="h-screen bg-black">
+      <div className="h-[90%] flex">
+        <SideBar />
+        <Display/>
+        
+      </div>
+      <div><Player/>
+      <audio  ref ={audioref} src={track.file} preload='auto'></audio></div>
+      
+      
+    </div>
+  )
+}
+
+export default App;
---
+++ src/components/Player.jsx
@@ -0,0 +1,48 @@
+import React, { useContext } from 'react';
+import { assets} from '../assets/assets';
+import { PlayerContext } from '../context/PlayerContext';
+const Player=()=>{
+
+
+    const {track,seekBar,seekBg,playStatus,play,pause,time,seekSong,previous,next}=useContext(PlayerContext);
+    return(
+        <div className='h-full bg-black flex justify-between items-center text-white px-4'>
+            <div className='lg:flex items-center gap-4'>
+                <img  className=' w-12' src={track.image} alt="" />
+            
+            <div>
+                <p >{track.name}</p>
+                <p>{track.desc.slice(0,12)}</p>
+            </div>
+            </div>
+            <div className='flex flex-col items-center gap-1 m-auto'>
+                <div className='flex gap-4'>
+                <img  className= "w-4 cursor-pointer"src={assets.shuffle_icon}></img>
+                <img  onClick={ previous} className= "w-4 cursor-pointer"src={assets.prev_icon}></img>
+                    {playStatus ?<img   onClick={pause} className= "w-4 cursor-pointer"src={assets.pause_icon}></img>
+                    :<img   onClick={play} className= "w-4 cursor-pointer"src={assets.play_icon}></img>}
+                    <img onClick={next} className= "w-4 cursor-pointer"src={assets.next_icon}></img>
+                    <img  className= "w-4 cursor-pointer"src={assets.loop_icon}></img>
+                </div>
+                <div className='flex items-center gap-5'>
+                    <p>{time.currentTime.minute}:{time.currentTime.second}</p>
+                    <div  ref={seekBg} onClick={seekSong} className='w-[60vw] max-w-[500px] bg-gray-300 rounded-full cursor-pointer'>
+                        <hr  ref={seekBar}className='h-1 border-none w-0 bg-green-800 rounded-full'/>
+                    </div>
+                    <p>{time.totalTime.minute}:{time.totalTime.second}</p>
+                </div>
+            </div>
+            <div className='hidden lg:flex items-center gap-2 opacity-75'>
+                <img className='w-4' src={assets.play_icon}></img>
+                <img className='w-4' src={assets.mic_icon}></img>
+                <img className='w-4' src={assets.queue_icon}></img>
+                <img className='w-4' src={assets.speaker_icon}></img>
+                <img className='w-4' src={assets.volume_icon}></img>
+                <div className='w-20 bg-slate-50 h-1 rounded-full'></div>
+                <img className='w-4' src={assets.mini_player_icon}></img>
+                <img className='w-4' src={assets.zooom_icon}></img>
+            </div>
+        </div>
+    )
+}
+export default Player;
